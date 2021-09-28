115526207fe1239dfc5e45c306bff38510b5f5d6
pracowniaElektron <damiander2004@gmail.com>
2021-09-28 12:08:04 +0200

    doxygen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+// copyright
+
 #include <iostream>
 
 using namespace std;
+
+/// @brief Główna funkcja programu, wyświetla fajny komunikat ...
+///
+/// @return zawsze zwraca wartość 0.
+
 
 int main(){
     cout<<"Hello World";
